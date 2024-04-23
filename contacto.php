6983115c398977294c50b86d4c179c6f28352b42
JuanM-Ortiz <juanmanuel.ortiz.vaz@gmail.com>
2024-04-23 13:37:53 -0300

    Contact View

--- contacto.php
+++ contacto.php
@@ -18,7 +18,66 @@
     <?php include_once 'modules/mobile-navbar.html'; ?>
 
     <section>
-        <h2 class="text-center text-white">CONTACTO</h2>
+        <div class="bg-gray row mt-4 py-4">
+            <h2 class="text-center text-white">CONTACTO</h2>
+        </div>
+
+        <div class="container my-5">
+
+            <form class="contact-form">
+                <div class="row mb-3">
+
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label text-white fw-bold fs-6">Nombre:</label>
+                            <input type="text" class="form-control rounded bg-gray" id="nombre" name="nombre" placeholder="Nombre">
+                        </div>
+                        <div class="mb-3 ">
+                            <label for="apellido" class="form-label text-white fw-bold fs-6 mt-3">Apellido:</label>
+                            <input type="text" class="form-control rounded bg-gray" id="apellido" name="apellido" placeholder="Apellido">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label text-white fw-bold fs-6 mt-3">Dirección de Correo Electrónico:</label>
+                            <input type="email" class="form-control rounded bg-gray" id="email" name="email" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="celular" class="form-label text-white fw-bold fs-6 mt-3">Celular:</label>
+                            <input type="tel" class="form-control rounded bg-gray" id="celular" name="celular" placeholder="Celular">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="asunto" class="form-label text-white fw-bold fs-6">Asunto:</label>
+                            <input type="text" class="form-control rounded bg-gray" id="asunto" name="asunto" placeholder="Asunto">
+                        </div>
+                        <div class="mb-3">
+                            <label for="mensaje" class="form-label text-white fw-bold fs-6 mt-1">Mensaje:</label>
+                            <textarea class="form-control rounded bg-gray" id="mensaje" name="mensaje" rows="10" placeholder="Escribe un mensaje..."></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary fw-bold fs-5">Enviar</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="bg-gray py-5">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 contact-img px-5">
+                        <img src="assets/img/main1.jpg" alt="Descripción de la imagen" class="img-fluid">
+                    </div>
+
+                    <div class="col-md-6 text-white fw-bold fs-5 px-5">
+                        <h3 class="text-center fw-bold">Contacto</h3>
+                        <p class="text-white fs-5 fw-bold">El formulario que presentamos a continuación ha sido diseñado para asegurar un vínculo efectivo con nuestros clientes. Nuestro equipo de soporte al cliente se encargará de procesar sus consultas o requerimientos de forma diligente. Por favor, llene los siguientes campos con su información. Apreciamos mucho su colaboración.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
     <?php include_once 'modules/footer-copyright.html' ?>
